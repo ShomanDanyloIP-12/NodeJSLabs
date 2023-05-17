ca0d4c988bec96ec44b197abfbe3bae39b6251c2
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2023-05-17 12:10:42 +0300

    Lab1 task 2 completed

--- Lab1.js
+++ Lab1.js
@@ -13,3 +13,12 @@ function add(n) {
 
 console.log(add(2)(5)(7)(1)(6)(5)(11)()); // 37
 console.log(add(2)(5)(7)(1)(6)(5)(11)(3)()); // 40
+
+function areAnagrams(stringA, stringB) {
+    let sortedStringA = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+    let sortedStringB = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+    return sortedStringA === sortedStringB;
+}
+
+console.log(areAnagrams('shoman', 'hmanos')); // True
+console.log(areAnagrams('anagram', 'alagram')); // False
