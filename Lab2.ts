4f5b8427073676b8c1c8f34b9e75745837aab243
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2023-05-18 09:10:53 +0300

    Lab2 task 2 completed

--- Lab2.ts
+++ Lab2.ts
@@ -13,3 +13,12 @@ function add(a: number): Add {
 }
 console.log(add(2)(5)(7)(1)(6)(5)(11)()); // 37
 console.log(add(2)(5)(7)(1)(6)(5)(11)(3)()); // 40
+
+function areAnagrams(s1: string, s2: string): boolean {
+    const normalize = (str: string) =>
+        str.toLowerCase().split('').sort().join('');
+    return normalize(s1) === normalize(s2);
+}
+
+console.log(areAnagrams('shoman', 'hmanos'));  // true
+console.log(areAnagrams('anagram', 'alagram'));  // false
