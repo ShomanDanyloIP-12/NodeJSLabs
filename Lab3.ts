10d27bd0d0b2adce4b64bab85a0627b1c5dbc00d
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2023-05-21 14:05:08 +0300

    Lab3 task 2 completed

--- Lab3.ts
+++ Lab3.ts
@@ -20,3 +20,20 @@ async function runSequent<T, U>(
   console.log(results); //{ item: 'one', index: 0 },{ item: 'two', index: 1 },{ item: 'three', index: 2 }
 })();
 
+function arrayChangeDelete<T>(array: T[], deleteRule: (item: T) => boolean): T[] {
+  const deletedElements: T[] = [];
+  
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (deleteRule(array[i])) {
+      deletedElements.push(array[i]);
+      array.splice(i, 1);
+    }
+  }
+  
+  return deletedElements;
+}
+
+const array = [1, 2, 3, 6, 7, 9];
+const deletedElements = arrayChangeDelete(array, (item) => item % 2 === 0);
+console.log(array); // [1, 3, 7, 9]
+console.log(deletedElements); // [6, 2]
