53fe92574d8e2bffa5c5352b0954fa9c6e2e7486
ShomanDanyloIP-12 <shomdan2004@gmail.com>
2023-05-21 23:17:14 +0300

    Lab3 task 3 completed

--- Lab3.ts
+++ Lab3.ts
@@ -37,3 +37,46 @@ const array = [1, 2, 3, 6, 7, 9];
 const deletedElements = arrayChangeDelete(array, (item) => item % 2 === 0);
 console.log(array); // [1, 3, 7, 9]
 console.log(deletedElements); // [6, 2]
+
+
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+async function fetchAndSaveHTMLContent(url: string, outputFilePath: string) {
+  try {
+    const response = await axios.get(url);
+    const htmlContent = response.data;
+    fs.writeFileSync(outputFilePath, htmlContent);
+    console.log(`Saved HTML content for ${url} to ${outputFilePath}`);
+  } catch (error) {
+    console.error(`Failed to fetch or save HTML content for ${url}:`, error);
+  }
+}
+
+async function processJSONFile(jsonFilePath: string) {
+  try {
+    const jsonData = fs.readFileSync(jsonFilePath, 'utf-8');
+    const links = JSON.parse(jsonData);
+    const jsonFileName = path.basename(jsonFilePath, '.json');
+    const outputDir = `${jsonFileName}_pages`;
+    
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+    
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const outputFilePath = path.join(outputDir, `page_${i + 1}.html`);
+      await fetchAndSaveHTMLContent(link, outputFilePath);
+    }
+    
+    console.log('Processing completed successfully.');
+  } catch (error) {
+    console.error('An error occurred while processing the JSON file:', error);
+  }
+}
+
+const jsonFilePath = path.join(__dirname, 'list.json');
+
+processJSONFile(jsonFilePath);
